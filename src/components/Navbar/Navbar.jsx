bc2f0a1a40cf78602c6f1c56fa67413f9d4da355
faeiz-furqan17 <faeiz.furqan@arbisoft.com>
2024-06-27 17:50:22 +0500

    folder structure

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import styles from './Navbar.module.scss';
+import Form from '../Form/Form';
+
+function Navbar() {
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
+  const handleAddClick = () => {
+    setIsFormVisible(true);
+  };
+
+  const handleCloseForm = () => {
+    setIsFormVisible(false);
+  };
+
+  return (
+    <div>
+      <header>
+        <div className={styles.navbar}>
+          <div className={styles.logo}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV4-xjZQP_eKyqr_bXBBoqGfKsmVNdYS8mcw&s" alt="Logo" />
+          </div>
+          <div className={styles.links}>
+            <ul>
+              <li>
+                <button onClick={handleAddClick}>
+                  Add
+                </button>
+              </li>
+              <li>
+                <button>
+                  <a href="">Dashboard</a>
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+      {isFormVisible && <Form onClose={handleCloseForm} />}
+    </div>
+  );
+}
+
+export default Navbar;
