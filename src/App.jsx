5d5728d750de5c21ffd4b5a07ce9a20ea09a3f80
faeiz-furqan17 <faeiz.furqan@arbisoft.com>
2024-06-28 14:29:42 +0500

    i was practicing useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Navbar from './components/Navbar/Navbar'
 import Lander from './components/Lander/Lander'
 import Form from './components/Form/Form'
 import SimpleForm from './components/SimpleForm/SimpleForm'
+import StudentDetailsForm from './components/StudentDetailsForm/StudentDetailsForm'
 
 
 
 function App() {
+const [showValue,hide] = useState(false)
+const [showStudentValue,hideStudent] = useState(false)
+const  showTheStudentForm=() =>{
+    hideStudent(!showStudentValue);
+  }
+  const  showTheForm=() =>{
+    hide(!showValue);
+  }
   return <>
   <div className="main-container">
 <Navbar>
@@ -16,9 +25,19 @@ function App() {
 <Lander>
 
 </Lander>
-<SimpleForm>
-  
+<button onClick={showTheForm}>
+  click me to make form visible
+</button>
+<SimpleForm show={showValue}>
+
 </SimpleForm>
+<button onClick={showTheStudentForm}>
+  click me to make student form visible
+</button>
+<StudentDetailsForm show={showStudentValue}>
+
+</StudentDetailsForm>
+
 
   </div>
   </>
--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -1,47 +1,64 @@
 import React from 'react'
 import { useState } from 'react'
-function SimpleForm() {
-const[name,setName]=useState('')
-const[email,setEmail]=useState('')
-const[password,setPassword]=useState('')
+function SimpleForm(props) {
+    
+    const [formData, setData] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(formData)
+    }
+    const handleChange = (e) => {
+        
+        const { name, value } = e.target
+        setData({
+           ...formData,
+            [name]: value
+        })
 
-const handleEmailChange=(e)=>{
-setEmail(e.target.value)
-}
+    }
+    
 
-const handlePasswordChange=(e)=>{
-setPassword(e.target.value)
-}
 
-const handleNameChange=(e)=>{
-setName(e.target.value)
-}
 
-const handleSubmit=(e)=>{
-e.preventDefault()
-console.log(name)
+// const[name,setName]=useState('')
+// const[email,setEmail]=useState('')
+// const[password,setPassword]=useState('')
 
-console.log(email)
+// const handleEmailChange=(e)=>{
+// setEmail(e.target.value)
+// }
 
-console.log(password)
-}
+// const handlePasswordChange=(e)=>{
+//     debugger
+// setPassword(e.target.value)
+// }
+
+// const handleNameChange=(e)=>{
+// setName(e.target.value)
+// }
+
   return (
-    <div>
+    props.show ? <div>
         <section className='form-section'>
         <h1>Simple Form</h1>
+
         <form onSubmit={handleSubmit}>
             <label htmlFor='name'>Name</label>
-            <input type='text' name='name' id='name'  value={name} onChange={handleNameChange}/>
-            <label htmlFor='email'>Email</label>
-            <input type='email' name='email' id='email' value={email} onChange={handleEmailChange} />
-            <label htmlFor='password'>Password</label>
-            <input name='password' id='password' value={password} onChange={handlePasswordChange} />
+                <input type='text' name='name' id='name'  value={formData.name} onChange={handleChange}/>
+                <label htmlFor='email'>Email</label>
+                <input type='email' name='email' id='email' value={formData.email} onChange={handleChange} />
+                <label htmlFor='password'>Password</label>
+                <input name='password' id='password' value={formData.password} onChange={handleChange} />
             <input type='submit' value='Submit' />
         </form>
 
         </section>
       
-    </div>
+    </div> : <></>
   )
 }
 
