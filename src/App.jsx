50c5034d9535943a05263fe849875b648e0d3ccb
faeiz-furqan17 <faeiz.furqan@arbisoft.com>
2024-07-03 13:41:20 +0500

    Implemented a simple login page and how to use protected routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
 import React, { useState } from "react";
 
-import Navbar from "./components/Navbar/Navbar";
-import Lander from "./components/Lander/Lander";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import SimpleForm from "./components/SimpleForm/SimpleForm";
-import StudentDetailsForm from "./components/StudentDetailsForm/StudentDetailsForm";
-import Parent from "./components/Parent/Parent";
-import Controlled from "./components/Controlled/Controlled";
-import Uncontrolled from "./components/Uncontrolled/Uncontrolled";
+
 import Layout from "./components/Layout/Layout";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Summary from "./components/Summary/Summary";
+import Login from "./components/Login/Login";
+import ProtectedRoutes from "./utils/ProtectedRoutes";
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [showValue, hide] = useState(false);
   const [showStudentValue, hideStudent] = useState(false);
-  const showTheStudentForm = () => {
-    hideStudent(!showStudentValue);
+
+  const handleLogin = () => {
+    setIsAuthenticated(true);
   };
-  const showTheForm = () => {
-    hide(!showValue);
-  };
+
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<Login login={handleLogin} />}></Route>
         <Route path="/" element={<Layout />}>
           <Route index element={<SimpleForm />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="summary" element={<Summary />} />
+          <Route element={<ProtectedRoutes user={isAuthenticated} />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="summary" element={<Summary />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
